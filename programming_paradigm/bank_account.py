6566d25ed20723c4c2e504ad290fae315ff42500
Bemgba-Apeagee <140463947+Bemgba-Apeagee@users.noreply.github.com>
2025-10-07 15:36:35 +0100

    Add files via upload

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,4 +23,4 @@ class BankAccount:
 
     def display_balance(self):
         """Display the current account balance."""
-        print(f"Current balance: ${self.current_balance:.2f}")
+        print(f"Current Balance: ${self.current_balance:.2f}")
